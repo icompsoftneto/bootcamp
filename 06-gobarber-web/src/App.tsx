513259b2a2e0b687628b80f72f2dcabca5e7a188
netocajaiba <neto.cajaiba@icompsoft.com.br>
2021-08-31 17:53:41 -0300

    modulo 8

--- 06-gobarber-web/src/App.tsx
+++ 06-gobarber-web/src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
-// import SignIn from './pages/SignIn';
-import SignUp from './pages/SignUp';
+import SignIn from './pages/SignIn';
+// import SignUp from './pages/SignUp';
 import GlobalStyle from './styles/global';
+import {AuthProvider} from './context/AuthContext';
 
 const App: React.FC = () => (
   <>
-  <SignUp />
+  <AuthProvider>
+    <SignIn />
+  </AuthProvider>
+
   <GlobalStyle />
   </>
 );
--- 06-gobarber-web/src/pages/SignIn/index.tsx
+++ 06-gobarber-web/src/pages/SignIn/index.tsx
@@ -1,21 +1,57 @@
-import React from "react";
+import React, {useRef, useCallback, useContext} from "react";
 import {FiLogIn, FiMail, FiLock} from 'react-icons/fi';
+import { FormHandles } from "@unform/core";
+import { Form } from "@unform/web";
+import * as Yup from 'yup';
+import { ValidationError } from "yup";
+import getValidationErrors from "../../utils/getValidationErrors";
 import logoImg from '../../assets/logo.svg';
 import Input from "../../components/Input";
 import Button from "../../components/Button";
 import { Container, Background, Content } from "./styles";
+import {AuthContext} from "../../context/AuthContext";
 
-const SignIn: React.FC = () => (
-    <Container>
+interface SignInFormData {
+    email: string;
+    password: string;
+}
+
+const SignIn: React.FC = () => {
+    const formRef = useRef<FormHandles>(null);
+    const {signIn} = useContext(AuthContext);
+    const handleSubmit = useCallback(async (data: SignInFormData) => {
+        try {
+            formRef.current?.setErrors({});
+            const schema = Yup.object().shape({
+                email: Yup.string().required('E-mail obrigatório').email('Digite um e-mail válido'),
+                password: Yup.string().required('Senha obrigatória'),
+            });
+            await schema.validate(data, {
+                abortEarly: false,
+            });     
+            
+            signIn({
+                email: data.email,
+                password: data.password,
+            });
+        } catch (err) {
+            console.log(err);
+            const errors = getValidationErrors(err as ValidationError);
+            formRef.current?.setErrors(errors);
+        }
+    }, [signIn], );
+
+    return (
+     <Container>
         <Content>
             <img src={logoImg} alt="GoBarber" />
-            <form>
-                <h1>Faça seu Logon</h1>
+            <Form ref={formRef} onSubmit={handleSubmit}>
+                <h1>Login</h1>
                 <Input name="email" icon={FiMail} placeholder="E-mail" />
                 <Input name="password" icon={FiLock} type="password" placeholder="Senha" />
                 <Button type="submit">Entrar</Button>
                 <a href="forgot">Esqueci minha senha</a>
-            </form>
+            </Form>
 
             <a href="login">
                 <FiLogIn />
@@ -26,5 +62,5 @@ const SignIn: React.FC = () => (
         <Background></Background>
     </Container>
 );
-
+    }
 export default SignIn;
